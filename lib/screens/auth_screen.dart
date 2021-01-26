68d0c386379851cf1cc2321fb5c88d3ca0ebc7db
DeleMike <akindelemichael65@gmail.com>
2021-01-26 15:01:12 +0100

    add content

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -3,8 +3,13 @@ import 'package:flutter/material.dart';
 class AuthScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Smart Church'),
+      ),
+      body: Center(
+        child: Text('Smart church Auth Pages coming...')
+      ),
     );
   }
 }
