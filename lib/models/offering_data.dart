74da5f4aa4b4a0c35df5d81ac3750c9b6a6fae82
DeleMike <akindelemichael65@gmail.com>
2021-01-28 16:21:19 +0100

    add offering model

--- lib/models/offering_data.dart
+++ lib/models/offering_data.dart
@@ -1,14 +1,14 @@
-import 'package:flutter/foundation.dart';
-
-///offering model toshow how much money collected at a particular month
+///offering model to show how much money collected at a particular month
 class OfferingData {
   ///Month of the year
   final String month;
+
   ///Amount collected
   final double amount;
 
-  OfferingData({
-    @required this.month,
-    @required this.amount,
-  }); 
+///offering model to show how much money collected at a particular month
+  OfferingData(
+    this.month,
+    this.amount,
+  );
 }
